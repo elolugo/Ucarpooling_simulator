2ab5bbad966f5e3866a779052fe615d323f80366
Alejandro Lugo <elo.lugo@gmail.com>
2019-11-26 17:24:18 -0300

    Finished distributing cars

--- scripts/distribute_cars.py
+++ scripts/distribute_cars.py
@@ -1,32 +1,27 @@
-import requests
-import json
 import csv
 import sys
 import os
-import shutil
-import googlemaps
-from datetime import datetime
+import random
 
 
 CURRENT_DIRECTORY = os.getcwd()
 CSV_FORMDATA_INPUT_FILE_PATH = "source_data\Formulario.csv"
 CSV_USERDATA_INPUT_FILE_PATH = "source_data\Alumnos_Campus_Geocoded.csv"
-CSV_OUTPUT_FILE_PATH = "output_data\Alumnos_profiles.csv"
+CSV_OUTPUT_FILE_PATH = "output_data\Alumnos_profiles_car_ownership.csv"
+CSV_CLEANED_FILE_PATH = "output_data\Alumnos_Campus_Geocoded_CLEANED.csv"
 
 
 def clean():
 
     """ Limpia los archivos de las coordenadas que estan fuera de Asuncion"""
-    path = os.path.join(CURRENT_DIRECTORY, CSV_USERDATA_INPUT_FILE_PATH)
 
-    with open(CSV_FORMDATA_INPUT_FILE_PATH, newline='', encoding='utf-8') as csv_input_formdata, \
-         open(CSV_USERDATA_INPUT_FILE_PATH, newline='', encoding='utf-8') as csv_input_userdata, \
-         open(CSV_OUTPUT_FILE_PATH, 'w', newline='', encoding='utf-8') as csv_output_file:
+    with open(CSV_USERDATA_INPUT_FILE_PATH, newline='', encoding='utf-8') as csv_input_userdata, \
+         open(CSV_CLEANED_FILE_PATH, 'w', newline='', encoding='utf-8') as csv_output_file:
 
         row_reader = csv.DictReader(csv_input_userdata, delimiter=';')
 
         # preparing the csv file for success geocoding rows
-        fieldnames = ['UUID', 'SEXO', 'CARRERA', 'DIRECCION', 'LONGITUD', 'LATITUD']
+        fieldnames = ['UUID', 'SEXO', 'CARRERA', 'DIRECCION', 'LATITUD', 'LONGITUD']
         output_csv_writer = csv.DictWriter(csv_output_file, fieldnames=fieldnames, delimiter=';')
         output_csv_writer.writeheader()
 
@@ -39,8 +34,9 @@ def clean():
             alumni_sex = row['SEXO']
             alumni_career = row['CARRERA']
             alumni_address = row['DIRECCION']
-            alumni_longitude = float(row['LONGITUD'])
             alumni_latitude = float(row['LATITUD'])
+            alumni_longitude = float(row['LONGITUD'])
+
 
             if -25.084644 > alumni_latitude > -25.541974 and -57.325033 > alumni_longitude > -57.680855:
                 print(f'{alumni_uuid} esta en py')
@@ -52,8 +48,8 @@ def clean():
                     'SEXO': alumni_sex,
                     'CARRERA': alumni_career,
                     'DIRECCION': alumni_address,
-                    'LATITUD': alumni_longitude,
-                    'LONGITUD': alumni_latitude
+                    'LATITUD': alumni_latitude,
+                    'LONGITUD': alumni_longitude
                     }
                 )
 
@@ -64,103 +60,78 @@ def clean():
         print(f'En Paraguay estan {py} personas')
         print(f'No estan en Py {nopy} personas')
 
+
+
+
 def distribute_mean_transportation():
 
-    with open(CSV_FORMDATA_INPUT_FILE_PATH, newline='', encoding='utf-8') as csv_input_formdata, \
-         open(CSV_USERDATA_INPUT_FILE_PATH, newline='', encoding='utf-8') as csv_input_userdata, \
+    with open(CSV_FORMDATA_INPUT_FILE_PATH, newline='', encoding='ascii', errors='ignore') as csv_input_formdata:
+
+        row_reader = csv.DictReader(csv_input_formdata, delimiter=';')
+
+        car_count = 0
+        not_car_count= 0
+
+        for row in row_reader:  # For each row in the original CSV
+            mean_transportation = row['MODO_TRANSPORTE']
+            if (mean_transportation == 'Manejo en auto'):
+                car_count = car_count + 1
+            elif (mean_transportation == 'Me traen en auto'):
+                car_count = car_count + 1
+            else:
+                not_car_count = not_car_count + 1
+
+        car_ownership_percentage = car_count / (car_count+not_car_count)
+        not_car_ownership_percentage = 1 - car_ownership_percentage
+        print(f'Hay {car_count+not_car_count} personas en el registro')
+        print(f'Hay {car_count} personas que tienen auto contando con el {car_ownership_percentage*100}')
+        print(f'Hay {not_car_count} personas que no tienen auto contando con el {not_car_ownership_percentage*100}')
+
+    """Distribute the car and not car ownership"""
+    with open(CSV_USERDATA_INPUT_FILE_PATH, newline='', encoding='utf-8') as csv_input_userdata, \
          open(CSV_OUTPUT_FILE_PATH, 'w', newline='', encoding='utf-8') as csv_output_file:
 
-        row_reader = csv.DictReader(csv_input, delimiter=';')
 
-        #preparing the csv file for success geocoding rows
-        fieldnames = ['UUID','SEXO','CARRERA','DIRECCION','LATITUD','LONGITUD']
-        output_csv_writer = csv.DictWriter(csv_output, fieldnames=fieldnames, delimiter=';')
+        row_reader = csv.DictReader(csv_input_userdata, delimiter=';')
+
+        """Writing the headers of the output file"""
+        fieldnames = ['UUID', 'MEANS_TRANSPORTATION']
+        output_csv_writer = csv.DictWriter(csv_output_file, fieldnames=fieldnames, delimiter=';')
         output_csv_writer.writeheader()
 
-        # preparing the csv file for failed geocoding rows
-        fieldnames = ['UUID', 'SEXO', 'CARRERA', 'DIRECCION']
-        output_failed_csv_writer = csv.DictWriter(csv_output_failed, fieldnames=fieldnames, delimiter=';')
-        output_failed_csv_writer.writeheader()
-
-
-        for row in row_reader: #For each row in the original CSV
-
+        total_cars = 0
+        total_rows = 0
+        for row in row_reader:  # For each row in the original CSV
+            total_rows = total_rows + 1
             alumni_uuid = row['UUID']
-            alumni_sex = row['SEXO']
-            alumni_career = row['CARRERA']
-            alumni_address = row['DIRECCION']
 
+            # Random assign according to distribution
+            if random.random() <= car_ownership_percentage:
+                # it has a car
+                total_cars = total_cars + 1
 
-            if(alumni_address[0]=='-'):#check if it has latitude and longitude already
-                # Geocoded already
-                print(alumni_uuid + " Geocoded already")
-
-                #get the latitude and longitude
-                latitudeAndLongitude = alumni_address.split(',')
-
-                json_dict = {
-                    'UUID': alumni_uuid,
-                    'SEXO': alumni_sex,
-                    'CARRERA': alumni_career,
-                    'DIRECCION': "Geocoded Address",
-                    'LATITUD': latitudeAndLongitude[0],
-                    'LONGITUD': latitudeAndLongitude[1].strip()
-                }
-                output_csv_writer.writerow(json_dict)
-
-            else: # Not Geocoded already
-
-                #Google API query
-                geocode_result = gmaps.geocode(alumni_address)
-
-                if not geocode_result:  # Check if the Geocode response is empty
-                    # Geocode response is empty
-                    print(alumni_uuid + " Geocode does not exist")
-                    # save into failed csv
-
-                    json_dict = {
+                output_csv_writer.writerow(
+                    {
                         'UUID': alumni_uuid,
-                        'SEXO': alumni_sex,
-                        'CARRERA': alumni_career,
-                        'DIRECCION': alumni_address
+                        'MEANS_TRANSPORTATION': 'Car',
                     }
+                )
 
-                    #write into the failed address csv file
-                    output_failed_csv_writer.writerow(json_dict)
+            else:
 
-                else:
-                    # Geocode response is not empty
-                    print(alumni_uuid + " Geocode does exist")
-
-
-                    # save into output csv
-                    latitude = geocode_result[0]['geometry']['location']['lat']
-                    longitude = geocode_result[0]['geometry']['location']['lng']
-
-                    json_dict = {
+                output_csv_writer.writerow(
+                    {
                         'UUID': alumni_uuid,
-                        'SEXO': alumni_sex,
-                        'CARRERA': alumni_career,
-                        'DIRECCION': alumni_address,
-                        'LATITUD': latitude,
-                        'LONGITUD': longitude
+                        'MEANS_TRANSPORTATION': 'No car',
                     }
+                )
 
-                    output_csv_writer.writerow(json_dict)
-
-
-                    # save into json list
-                    json_dict = {
-                        'UUID': alumni_uuid,
-                        'SEXO': alumni_sex,
-                        'CARRERA': alumni_career,
-                        'RESPONSE': geocode_result[0]
-                    }
-                    json_responses_list.append(json_dict)
-
-        #Dump all the json responses in a json file
-        json.dump(json_responses_list, json_output, ensure_ascii=False, indent=4)
+        print(f'EN TOTAL DE {total_rows}, {total_cars} TINEN AUTO QUE CONSTA EL {(total_cars/total_rows)*100}%')
 
 
 if __name__ == "__main__":
-    clean()
+    #clean()
+    distribute_mean_transportation()
+
+
+
