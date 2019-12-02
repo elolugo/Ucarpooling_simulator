d01e8d2c3c1be6282d9b7dfccc4ba04b31aa4660
Alejandro Lugo <elo.lugo@gmail.com>
2019-12-01 21:00:16 -0300

    Refactored distribution code for dynamism

--- scripts/distribute_cars.py
+++ scripts/distribute_cars.py
@@ -1,5 +1,5 @@
 import csv
-import random
+
 
 from scripts import settings
 from scripts import helper
@@ -11,32 +11,10 @@ def distribute_mean_transportation():
 
         row_reader = csv.DictReader(csv_input_formdata, delimiter=settings.FORM_DELIMITER)
 
-        car_count = 0
-        not_car_count= 0
+        total_mean_transportation, car_distribution = helper.count_distribution(settings.FIELDNAME_TRANSPORT, row_reader)
 
-        for row in row_reader:  # For each row in the original CSV
-
-            alumni = helper.get_alumni_form(row)
-            mean_transportation = alumni[settings.FIELDNAME_TRANSPORT]
-            if (mean_transportation == 'Car'):
-                car_count = car_count + 1
-            else:
-                not_car_count = not_car_count + 1
-
-        car_ownership_percentage = car_count / (car_count+not_car_count)
-        not_car_ownership_percentage = 1 - car_ownership_percentage
-        print(f'Hay {car_count+not_car_count} personas en el registro')
-        print(f'Hay {car_count} personas que tienen auto contando con el {car_ownership_percentage*100}%')
-        print(f'Hay {not_car_count} personas que no tienen auto contando con el {not_car_ownership_percentage*100}%')
-
-    """Count how many rows are to be assigned"""
-    with open(settings.CSV_USERDATA_INPUT_FILE_PATH, newline='', encoding='utf-8') as csv_input_userdata:
-
-        row_reader = csv.DictReader(csv_input_userdata, delimiter=settings.SAPIENTIA_DELIMITER)
-
-        total_rows = 0
-        for row in row_reader:  # For each row in the original CSV
-            total_rows = total_rows + 1
+    """Assign the range of probability for each option of values"""
+    helper.calculate_range_distribution(car_distribution)
 
     """Distribute the car and not car ownership"""
     with open(settings.CSV_USERDATA_INPUT_FILE_PATH, newline='', encoding='utf-8') as csv_input_userdata, \
@@ -50,27 +28,13 @@ def distribute_mean_transportation():
         output_csv_writer = csv.DictWriter(csv_output_file, fieldnames=output_csv_fieldnames, delimiter=';')
         output_csv_writer.writeheader()
 
-        total_cars = 0
-
-        for row in row_reader:  # For each row in the original CSV
-
-            alumni = helper.get_alumni_sapientia(row)
-
-
-            # Random assign according to distribution
-            if random.random() <= car_ownership_percentage:
-                # it has a car
-                total_cars = total_cars + 1
-
-                alumni[settings.FIELDNAME_TRANSPORT] = 'Car'
-
-            else:
-
-                alumni[settings.FIELDNAME_TRANSPORT] = 'No Car'
-
-            output_csv_writer.writerow(alumni)
-
-        print(f'EN TOTAL DE {total_rows}, {total_cars} TINEN AUTO QUE CONSTA EL {(total_cars/total_rows)*100}%')
+        """Actually apply the calculated distribution to the data"""
+        helper.assign_distribution_to_alumni_data(
+            row_reader=row_reader,
+            csv_writer=output_csv_writer,
+            distribution=car_distribution,
+            column=settings.FIELDNAME_TRANSPORT
+        )
 
 
 if __name__ == "__main__":
--- scripts/helper.py
+++ scripts/helper.py
@@ -4,7 +4,7 @@ Helper functions that are used throughout all files on the project
 
 import datetime
 from scripts import settings
-
+import random
 
 
 def get_alumni_sapientia(row):
@@ -40,3 +40,100 @@ def get_alumni_form(row):
     }
 
     return alumni
+
+
+def count_distribution(column_to_count, row_reader):
+    """
+    Counts the distribution of a column of the form data.
+    Returns the counts of values and the total record of the form data
+    """
+
+    total_records = 0
+
+    distribution = {}
+
+    for row in row_reader:  # For each row in the original CSV
+        total_records = total_records + 1
+
+        alumni = get_alumni_form(row)
+        record_value = alumni[column_to_count]
+
+        if record_value in distribution:
+            distribution[record_value] = distribution[record_value] + 1
+        else:
+            distribution[record_value] = 1
+
+    # Printing the results
+    print("==============================================================================")
+    print(f'total records in form data: {total_records}')
+    for key in distribution:
+
+        count = distribution[key]
+        distribution[key] = {
+            'count': count,
+            'percentage': count / total_records
+        }
+
+        print(
+            f'Total of {key} in the form is: {distribution[key]["count"]}  ------ percentage: {distribution[key]["percentage"]}%')
+
+    return total_records, distribution
+
+
+def calculate_range_distribution(distribution):
+    """
+    Assign the range of probability for each option of values.
+    """
+
+    ordered_options = sorted(distribution.items(), key=lambda kv: kv[1]['count'], reverse=False)
+
+    # Assign range for distribution
+    min = 0
+    max = 0
+    for item in ordered_options:
+        item_percentage = item[1]['percentage']
+        min = max
+        max = min + item_percentage
+
+        option = item[0]
+        distribution[option]['min'] = min
+        distribution[option]['max'] = max
+
+
+def assign_distribution_to_alumni_data(row_reader, csv_writer, distribution, column):
+    """
+    Actually apply the calculated distribution to the data
+    """
+
+    total_records = 0
+
+    for row in row_reader:  # For each row in the original CSV
+
+        alumni = get_alumni_sapientia(row)
+
+        # Generating random value from 0 to 1
+        random_value = random.random()
+
+        for means_tranport in distribution:
+
+            if distribution[means_tranport]['min'] < random_value <= distribution[means_tranport]['max']:
+
+                alumni[column] = means_tranport
+
+                csv_writer.writerow(alumni)
+
+                if 'total_distributed' in distribution[means_tranport]:
+                    distribution[means_tranport]['total_distributed'] += 1
+                else:
+                    distribution[means_tranport]['total_distributed'] = 1
+
+                total_records += 1
+
+                break
+
+    """Printing all the results"""
+    print("==============================================================================")
+    print(f'Total records assigned to the sapientia data: {total_records}')
+
+    for means_tranport in distribution:
+        print(f'{means_tranport} assigned: {distribution[means_tranport]["total_distributed"]} accounting for: {distribution[means_tranport]["total_distributed"] / total_records}%')
